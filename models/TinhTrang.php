a7bf66483b326c05dbc51f024875700cb1e60a22
congdanh1252 <70893956+congdanh1252@users.noreply.github.com>
2021-10-17 14:44:20 +0700

    Add correct model files

---
+++ models/TinhTrang.php
@@ -0,0 +1,22 @@
+<?php
+class TinhTrang {
+    private $MaTinhTrang;
+    private $TenTinhTrang;
+
+    function set_MaTinhTrang($ma) {
+        $this->MaTinhTrang = $ma;
+    }
+
+    function get_MaTinhTrang() {
+        return $this->MaTinhTrang;
+    }
+
+    function set_TenTinhTrang($ten) {
+        $this->TenTinhTrang = $ten;
+    }
+
+    function get_TenTinhTrang() {
+        return $this->TenTinhTrang;
+    }
+}
+?>
