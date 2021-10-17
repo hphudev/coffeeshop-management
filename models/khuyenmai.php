d991b952257d934d815be647780818b60a8c30af
congdanh1252 <70893956+congdanh1252@users.noreply.github.com>
2021-10-17 14:04:56 +0700

    Add models

---
+++ models/khuyenmai.php
@@ -0,0 +1,145 @@
+<?php
+class KhuyenMai {
+    private $MaKM;
+    private $TenKM;
+    private $ThoiGianBD;
+    private $ThoiGianKT;
+    private $SoLuong;
+
+    function set_MaKM($ma) {
+        $this->$MaKM = $ma;
+    }
+
+    function get_MaKM() {
+        return $this->$MaKM;
+    }
+
+    function set_TenKM($ten) {
+        $this->$TenKM = $ten;
+    }
+
+    function get_TenKM() {
+        return $this->$TenKM;
+    }
+
+    function set_ThoiGianBD($time) {
+        $this->$ThoiGianBD = $time;
+    }
+
+    function get_ThoiGianBD() {
+        return $this->$ThoiGianBD;
+    }
+
+    function set_ThoiGianKT($time) {
+        $this->$ThoiGianKT = $time;
+    }
+
+    function get_ThoiGianKT() {
+        return $this->$ThoiGianKT;
+    }
+
+    function set_SoLuong($sl) {
+        $this->$SoLuong = $sl;
+    }
+
+    function get_SoLuong() {
+        return $this->$SoLuong;
+    }
+}
+
+class ChiTietKhuyenMai_PhanTram {
+    private $MaKM;
+    private $MaCTKM;
+    private $PhanTramKM;
+    private $TienKMToiDa;
+    private $TienKMToiThieu;
+    private $TienHDToiThieu;    
+
+    function set_MaKM($ma) {
+        $this->$MaKM = $ma;
+    }
+
+    function get_MaKM() {
+        return $this->$MaKM;
+    }
+
+    function set_MaCTKM($ten) {
+        $this->$MaCTKM = $ten;
+    }
+
+    function get_MaCTKM() {
+        return $this->$MaCTKM;
+    }
+
+    function set_PhanTramKM($per) {
+        $this->$PhanTramKM = $per;
+    }
+
+    function get_PhanTramKM() {
+        return $this->$PhanTramKM;
+    }
+
+    function set_TienKMToiDa($amount) {
+        $this->$TienKMToiDa = $amount;
+    }
+
+    function get_TienKMToiDa() {
+        return $this->$TienKMToiDa;
+    }
+
+    function set_TienKMToiThieu($sl) {
+        $this->$TienKMToiThieu = $sl;
+    }
+
+    function get_TienKMToiThieu() {
+        return $this->$TienKMToiThieu;
+    }
+
+    function set_TienHDToiThieu($sl) {
+        $this->$TienHDToiThieu = $sl;
+    }
+
+    function get_TienHDToiThieu() {
+        return $this->$TienHDToiThieu;
+    }
+}
+
+class ChiTietKhuyenMai_Mon {
+    private $MaKM;
+    private $MaMon;
+    private $SoLuong;
+    private $TienHDToiThieu;    
+
+    function set_MaKM($ma) {
+        $this->$MaKM = $ma;
+    }
+
+    function get_MaKM() {
+        return $this->$MaKM;
+    }
+
+    function set_MaMon($ten) {
+        $this->$MaMon = $ten;
+    }
+
+    function get_MaMon() {
+        return $this->$MaMon;
+    }
+
+    function set_TienHDToiThieu($sl) {
+        $this->$TienHDToiThieu = $sl;
+    }
+
+    function get_TienHDToiThieu() {
+        return $this->$TienHDToiThieu;
+    }
+
+    function set_SoLuong($sl) {
+        $this->$SoLuong = $sl;
+    }
+
+    function get_SoLuong() {
+        return $this->$SoLuong;
+    }
+}
+?>
---
+++ models/mon.php
@@ -0,0 +1,122 @@
+<?php
+class Mon {
+    private $MaMon;
+    private $MaLoaiMon;
+    private $MaTinhTrang;
+    private $MaNVL;
+    private $MaDVT;
+    private $TenMon;
+    private $HinhAnh;
+    private $DonGia;
+    private $NgayThem;
+    private $MaKTM;
+    private $MoTa;
+    private $GhiChu;
+    private $NgayChinhSuaLanCuoi;
+
+
+    function set_MaMon($ma) {
+        $this->$MaMon = $ma;
+    }
+
+    function get_MaMon() {
+        return $this->$MaMon;
+    }
+
+    function set_MaLoaiMon($ma) {
+        $this->$MaLoaiMon = $ma;
+    }
+
+    function get_MaLoaiMon() {
+        return $this->$MaLoaiMon;
+    }
+
+    function set_MaTinhTrang($ma) {
+        $this->$MaTinhTrang = $ma;
+    }
+
+    function get_MaTinhTrang() {
+        return $this->$MaTinhTrang;
+    }
+
+    function set_MaNVL($ma) {
+        $this->$MaNVL = $ma;
+    }
+
+    function get_MaNVL() {
+        return $this->$MaNVL;
+    }
+
+    function set_MaDVT($ma) {
+        $this->$MaDVT = $ma;
+    }
+
+    function get_MaDVT() {
+        return $this->$MaDVT;
+    }
+
+    function set_TenMon($ten) {
+        $this->$TenMon = $ten;
+    }
+
+    function get_TenMon() {
+        return $this->$TenMon;
+    }
+
+    function set_HinhAnh($anh) {
+        $this->$HinhAnh = $anh;
+    }
+
+    function get_HinhAnh() {
+        return $this->$HinhAnh;
+    }
+
+    function set_DonGia($gia) {
+        $this->$DonGia = $gia;
+    }
+
+    function get_DonGia() {
+        return $this->$DonGia;
+    }
+
+    function set_NgayThem($ngay) {
+        $this->$NgayThem = $ngay;
+    }
+
+    function get_NgayThem() {
+        return $this->$NgayThem;
+    }
+
+    function set_NgayChinhSuaLanCuoi($ngay) {
+        $this->$NgayChinhSuaLanCuoi = $ngay;
+    }
+
+    function get_NgayChinhSuaLanCuoi() {
+        return $this->$NgayChinhSuaLanCuoi;
+    }
+
+    function set_MaKTM($ma) {
+        $this->$MaKTM = $ma;
+    }
+
+    function get_MaKTM() {
+        return $this->$MaKTM;
+    }
+
+    function set_MoTa($mt) {
+        $this->$MoTa = $mt;
+    }
+
+    function get_MoTa() {
+        return $this->$MoTa;
+    }
+
+    function set_GhiChu($note) {
+        $this->$GhiChu = $note;
+    }
+
+    function get_GhiChu() {
+        return $this->$GhiChu;
+    }
+}
+?>
